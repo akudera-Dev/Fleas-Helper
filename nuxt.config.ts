84fe4edb5812ecbb4ee58b9d57532a5d87422d7f
akudera <hjktive1516@gmail.com>
2025-12-02 21:05:48 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["@/styles/main.scss"],
+  app: {
+    head: {
+      title: "Fleas Helper",
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.png",
+        },
+      ],
+    },
+  },
+});
