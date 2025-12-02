19500dd4423424e742df977873f24eedbdc7d15e
akudera <hjktive1516@gmail.com>
2025-12-02 21:11:22 +0300

    add header

---
+++ app/components/fleas/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="header container">
+    <h1 class="title">SilkSong Fleas Helper</h1>
+    <span class="subtitle">Track missing fleas and view them on the interactive map</span>
+    <hr />
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 8px;
+  margin-top: 32px;
+}
+
+.title {
+  font-family: var(--font-accent);
+  font-weight: 700;
+  color: var(--color-accent);
+  text-align: center;
+}
+
+.subtitle {
+  font-family: var(--font-primary);
+  font-weight: 500;
+  color: var(--color-muted);
+  text-align: center;
+}
+
+hr {
+  width: 100%;
+  height: 1px;
+  margin-block: 40px 0;
+  border: none;
+  background-color: var(--color-accent);
+}
+</style>
---
+++ app/pages/fleas/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <FleasHeader />
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+body {
+  background-color: var(--color-root);
+  overflow: hidden;
+}
+</style>
