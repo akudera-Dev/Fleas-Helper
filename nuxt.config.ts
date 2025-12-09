1deae1fb46f7ef9027b17815bee01ddb80f9e1b4
akudera <hjktive1516@gmail.com>
2025-12-09 17:57:29 +0300

    chore: add gsc verification meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        {
+          name: "google-site-verification",
+          content: "r9rvLpe3DW-KVtzEcxfpC_Q9rzx2_lOMw5N_eL0iUTQ",
+        },
+      ],
+    },
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@pinia/nuxt"],
